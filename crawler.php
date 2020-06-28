6157019b6e1344c3aaf3bcc75faef212f389cb15
cheki <borna986@gmail.com>
2020-06-28 22:01:29 +0200

    optimized

--- crawler.php
+++ crawler.php
@@ -75,28 +75,29 @@ $doc = new DOMDocument();
 $title = $doc->getElementsByTagName("title");
 @$title = $title->item(0)->nodeValue;
 
-$header_links [] = _get_elements($doc,'meta');
-$metas   	  [] = _get_elements($doc,'link');
-$imgs 	 	  [] = _get_elements($doc,'img');
-$scripts 	  [] = _get_elements($doc,'script');
-$hrefs   	  [] = _get_elements($doc,'a');
-$forms 	 	  [] = _get_elements($doc,'form');
 
-$json = json_encode([$header_links,$metas,$hrefs,$imgs,$scripts,$forms]);
+$links	 = _get_elements($doc,'link');
+$metas   = _get_elements($doc,'meta');
+$imgs 	 = _get_elements($doc,'img');
+$scripts = _get_elements($doc,'script');
+$hrefs   = _get_elements($doc,'a');
+$forms 	 = _get_elements($doc,'form');
+
+$i_page = [$links,$metas,$hrefs,$imgs,$scripts,$forms];
+$json = json_encode($i_page);
 
 while(1){
-	$time = date("H:i:s");
 	print "\33[93mOptions:\ne -exit\np -print main webpage results\nf -follow webpage links\33[0m\n";
 	switch (readline("> ")) {
 	 	case 'p':
-			print_r(json_decode($json));
+			print_r($i_page);
 			echo "\n";
 			break;
-	 	
+
 		case 'e':
 			break 2;
 
-		case 'f':
+        case 'f':
 			follow_links($opts,$doc,$parsed_url['host'],$scheme,$prox_opt);
 			break;
 
@@ -114,8 +115,15 @@ exit(0);
 function follow_links($opts,$doc,$domain,$scheme,$prox_opt){
 	$links     = $doc->getElementsByTagName('a');
 	$url_regex = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,5}(\/\S*)?/";
-	$crawled   = $header_links = $metas = $hrefs = $imgs = $scripts = $forms = [];
-
+	$crawled   = $h_links = $metas = $hrefs = $imgs = $scripts = $forms = [];
+	$tagl = 'link';
+	$tagm = 'meta';
+	$tagi = 'img';
+	$tags = 'script';
+	$taga = 'a';
+	$tagf = 'form';
+	$cnt = 0;
+    
 	foreach ($links as $a)
 	{	
 		foreach ($a->attributes as $link)
@@ -124,14 +132,15 @@ function follow_links($opts,$doc,$domain,$scheme,$prox_opt){
 			$url  = trim($link->nodeValue);
 			$time = date("Y-m-d H:i:s");
 			// skip if link empty
+
+
 			if($url === '#' || empty($url)){
 				break;	
 			}
 
-
 			// TODO :: filter links-to-crawl
-
-
+			
+			
 			// do not download content!!!
 			else if(preg_match("/(\.(x{0,1})(apk$))|(\.ipa$)|\.mp{1}[(3{0,1})|(4{0,1})]$|\.jp(e{0,1})g$|(\.png$)/", $url))
 			{
@@ -151,14 +160,12 @@ function follow_links($opts,$doc,$domain,$scheme,$prox_opt){
 			if(in_array($url, $crawled)){
 			   break;  
 			}
+
 			$crawled [] = $url;
-			$log .= "\r\n[".$time."] > URL: ".$url."\r\n";
+			$log       .= "\r\n[".$time."] > URL: ".$url."\r\n";
 
 			if(preg_match($url_regex, $url) === 0){ 
 				print "\033[95m> ".$url." >>> Invalid link format..\n> Adapting..\033[0m \n";
-
-				// save link to relative file
-				file_put_contents(DATA_DIR."rel_".str_replace("/", "-", $domain).".txt", $url."\n", FILE_APPEND);
 
 				if($url[0] == "/" && substr($url, 0, 2) != "//")
 				{
@@ -184,16 +191,21 @@ function follow_links($opts,$doc,$domain,$scheme,$prox_opt){
 				else {
 					$url = $scheme."://".$domain."/".$url;
 				}
+
+				// save rel links
+				file_put_contents(DATA_DIR."rel_".str_replace("/", "-", $domain).".txt", $url."\n", FILE_APPEND);
+
 				echo "\33[32m> New url:  ".$url."\33[0m\n";
 				$log .= "[".$time."] > New url:  ".$url."\r\n";
 
 			} else {
+				// save abs links
 				file_put_contents(DATA_DIR."abs_".str_replace("/", "-", $domain).".txt", $url."\n", FILE_APPEND);
 			}
 
 			if(! check_base_domain($domain,$url,$scheme)){
-				$log .= "> Invalid base domain, skipping..\r\n";
-				print "\033[95m> Invalid base domain, skipping..\33[0m\n";
+				$log .= "> Invalid base domain [".$domain."], skipping..\r\n";
+				print "\033[95m> Invalid base domain [".$domain."], skipping..\33[0m\n";
 				continue;
 			}
 			$results = call__curl($scheme, $url, $prox_opt);
@@ -221,17 +233,22 @@ function follow_links($opts,$doc,$domain,$scheme,$prox_opt){
 			file_put_contents(DATA_DIR.str_replace("/", "-", $domain).".txt", $log, FILE_APPEND);
 		}
 
-		// TODO:: filter arrays!!
+		$h_links [$tagl.$cnt++] = _get_elements($doc,$tagl);
+		$metas   [$tagm.$cnt++] = _get_elements($doc,$tagm);
+		$imgs 	 [$tagi.$cnt++] = _get_elements($doc,$tagi);
+		$scripts [$tags.$cnt++] = _get_elements($doc,$tags);
+		$hrefs   [$taga.$cnt++] = _get_elements($doc,$taga);		
+		$forms 	 [$tagf.$cnt++] = _get_elements($doc,$tagf);
+    }
 
-		$header_links [] = _get_elements($doc,'meta');
-		$metas   	  [] = _get_elements($doc,'link');
-		$imgs 	 	  [] = _get_elements($doc,'img');
-		$scripts 	  [] = _get_elements($doc,'script');
-		$hrefs   	  [] = _get_elements($doc,'a');
-		$forms 	 	  [] = _get_elements($doc,'form');
+    $_h_links = array_unique($h_links, SORT_STRING);
+	$_metas   = array_unique($metas, SORT_STRING);
+	$_imgs 	  = array_unique($imgs, SORT_STRING);
+	$_scripts = array_unique($scripts, SORT_STRING);
+	$_hrefs	  = array_unique($hrefs, SORT_STRING);
+	$_forms   = array_unique($forms, SORT_STRING);
 
-	}
-	$json = json_encode([$header_links,$metas,$hrefs,$imgs,$scripts,$forms]);
+	$json = json_encode([$_h_links,$_metas,$_imgs,$_scripts,$_hrefs,$_forms]);
 	print "\33[96mList of crawled pages:\n";
 	print_r($crawled);
 	print "\33[0m";
@@ -289,7 +306,7 @@ function call__curl($scheme, $url, $prox_opt){
 	];
 }
 
-function select_opts($scheme,$url,$use_prox){
+function select_opts($scheme, $url, $prox_opt){
 	return [
 		CURLOPT_URL 			=> preg_match('/^'.$scheme.'./', $url) ? $url : $scheme."://".$url,
 	    CURLOPT_HEADER 			=> 1,
@@ -303,7 +320,7 @@ function select_opts($scheme,$url,$use_prox){
 	    CURLOPT_MAXREDIRS 		=> 5,
 		CURLOPT_COOKIEJAR		=> 'SessionCookies.txt',
 		CURLOPT_COOKIEFILE		=> 'SessionCookies.txt',
-		CURLOPT_PROXY 			=> $use_prox === 1 ? '127.0.0.1:9050' : false, 
+		CURLOPT_PROXY 			=> $prox_opt === 1 ? '127.0.0.1:9050' : false, 
 	    CURLOPT_PROXYTYPE		=> CURLPROXY_SOCKS5_HOSTNAME,
 	    CURLOPT_HTTPHEADER		=> [
 		    'User-Agent: '.setUserAgent(),
@@ -313,80 +330,66 @@ function select_opts($scheme,$url,$use_prox){
 	];
 }
 
-function check_base_domain($base_domain, $link_domain, $scheme){	
-
+function check_base_domain($base_domain, $link_domain, $scheme){
 	$link_domain = parse_url($link_domain);
-
 	if(substr($base_domain, 0,4) === "www."){
 		$base_domain = substr($base_domain,4);
 	}
-
 	if(substr($link_domain['host'], 0,4) === "www."){
 		$link_domain['host'] = substr($link_domain['host'],4);
 	}
-
 	// subdomain check
     if(strpos($link_domain['host'], $base_domain)){
     	return true;
     }
-
 	return ($base_domain === $link_domain['host']); 
 }
 
-function _get_elements($doc,$tag){
+function _get_elements($doc, $tag){
 	$eles = $doc->getElementsByTagName($tag);
 	if(empty($eles[0])){
 		return false;
 	}
+	$cnt = 0;
 	$elements = [
-		'Element' => $eles[0]->nodeName
+		'node' => $eles[0]->nodeName
 	];
 	if($tag === 'a'){
 		foreach ($eles as $a) {
 			foreach ($a->attributes as $attr) {
 				if(!empty($attr->nodeValue) && $attr->nodeName == "href" && $attr->nodeValue != "#"){
-					$elements [] = [
-						'Node' 	=> $attr->nodeName,
-						'Value' => $attr->nodeValue
-					];
+					$elements [$attr->nodeName.$cnt++] = $attr->nodeValue;
 				}
 			}							
 		}
 	} else if($tag === 'form'){
-		foreach ($eles as $form) {
-			foreach ($form->attributes as $attr) {
+		foreach ($eles as $key => $form){
+			$cnt = 0;
+			foreach ($form->attributes as $attr){
 				if(!empty($attr->nodeValue)){
-					$elements [] = [
-						'Node' 	=> $attr->nodeName,
-						'Value' => $attr->nodeValue
-					];
+					$elements [$attr->nodeName.$cnt++] = $attr->nodeValue;
 				}
 			}		
-			if ($form->hasChildNodes()) {
-				foreach($form->childNodes as $item){
-					$elements [] = [
-						'Child_node' => $item->nodeName,
-						'Value' 	 => $item->nodeValue
-					];
-					if($item->attributes !== null){
+			if ($form->hasChildNodes()){
+				$cnt = 0;
+				foreach($form->childNodes as $item)
+				{
+					$elements ['child-node-'.$item->nodeName.$cnt++] = $item->nodeValue;
+					if($item->attributes !== null)
+					{
+						$cnt = 0;
 						foreach($item->attributes as $attr){
-							$elements [] = [
-								'Child_attr' => $attr->nodeName,
-								'Value' 	 => $attr->nodeValue
-							];
+							$elements ['child-attr-'.$attr->nodeName.$cnt++] = $attr->nodeValue;
 						}
 					}
 				}  
 			}
 		}
 	} else {
-		foreach ($eles as $ele) {
-			foreach ($ele->attributes as $attr) {
+		foreach ($eles as $ele){
+			foreach ($ele->attributes as $attr){
 				if(!empty($attr->nodeValue)){
-					$elements [] = [
-						'Node' 	=> $attr->nodeName,
-						'Value' => $attr->nodeValue
-					];
+					$elements [$attr->nodeName.$cnt++] = $attr->nodeValue;
 				}
 			}				
 		}
@@ -395,33 +398,6 @@ function _get_elements($doc,$tag){
 }
 
 function setUserAgent(){
-	/*
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-	    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
-	    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-	    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-	    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-	    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-	    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-	    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
-	*/
     $agentBrowser = [
         'Firefox',
         'Safari',
@@ -440,7 +416,7 @@ function setUserAgent(){
     return $agentBrowser[rand(0,3)].'/'.rand(1,8).'.'.rand(0,9).'('.$agentOS[rand(0,6)].' '.rand(1,7).'.'.rand(0,9).'; en-US;)';
 }
 
-function publicIp($proxopt){
+function publicIp($prox_opt){
 	$ch = curl_init();
 
 	curl_setopt($ch,CURLOPT_URL, 'https://ipinfo.io/ip'); // alt-'https://httpbin.org/ip', 'ipv4.icanhazip.com'
@@ -451,7 +427,7 @@ function publicIp($proxopt){
 	    'User-Agent: '.setUserAgent(),
     	'Access-Control-Allow-Origin: null'
 	));
-    if($proxopt === 1){
+    if($prox_opt === 1){
 		curl_setopt($ch,CURLOPT_PROXY, '127.0.0.1:9050');
     }
 	$publicIp = curl_exec($ch);
